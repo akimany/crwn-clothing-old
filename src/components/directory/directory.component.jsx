f7bbf604d4bd4ce22c17dc9f52b1a6336c65cd45
akimany <akimany@gmail.com>
2022-02-19 08:58:01 +0000

    Homepage was broken into smaller components and styling menu components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import { sections } from '../../directory.data';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: sections,
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
